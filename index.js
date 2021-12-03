39c4156fdbed7267dd1cb223c7a640f6526efc34
shubhransh <shubhranshmeshram@gmail.com>
2021-12-03 12:37:16 +0530

    Set up Routes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const   express =  require('express');
 const   app     =  express();
 const   port    =  8000;
+const   router  =  require('./routes/home');
+
+app.use('/', router);
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error: ${err}`);
---
+++ routes/home.js
@@ -0,0 +1,10 @@
+const   express =  require('express');
+const   app     =  express();
+const   router  =  express.Router();
+
+console.log('Router loaded');
+
+
+
+
+module.exports = router;
